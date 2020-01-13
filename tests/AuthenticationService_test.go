718d9811285400dbd3699183e26b0d1f1d8d0fc9
Julian Chu <yulang.chu@gmail.com>
2020-01-13 23:58:53 +0800

    refactor TestMain

--- tests/AuthenticationService_test.go
+++ tests/AuthenticationService_test.go
@@ -3,30 +3,66 @@ package tests
 import (
 	"github.com/Julian-Chu/DependencyInjectionWorkshop/AuthenticationService"
 	"github.com/Julian-Chu/DependencyInjectionWorkshop/mocks"
+	"github.com/stretchr/testify/mock"
 	"os"
 	"testing"
 )
 
 var authService AuthenticationService.IAuthentication
 
+var failedCounter *mocks.IFailedCounter
+var profile *mocks.IProfile
+var hash *mocks.IHash
+
 func TestMain(m *testing.M) {
-	failedCounter := &mocks.IFailedCounter{}
-	failedCounter.On("GetLock", "joey").Return(false, nil)
-	failedCounter.On("AddFailedCount", "joey").Return(nil)
-	failedCounter.On("GetFailedCount", "joey").Return(0, nil)
-	failedCounter.On("Reset", "joey").Return(nil)
-	profile := &mocks.IProfile{}
-	profile.On("GetPasswordFromDB", "joey").Return("my hashed password", nil)
-	hash := &mocks.IHash{}
-	hash.On("Compute", "1234").Return("my hashed password", nil)
+	failedCounter = &mocks.IFailedCounter{}
+	name := "joey"
+	isLocked := false
+	var err error
+	GivenAccountIsLocked(name, isLocked, err)
+	GivenAddFailedCountReturn(name, err)
+	failedCount := 0
+	GivenFailedCount(name, failedCount, err)
+	GivenResetReturn(name, err)
+	profile = &mocks.IProfile{}
+	hashedPassword := "my hashed password"
+	GivenPasswordFromDB(name, hashedPassword, err)
+	hash = &mocks.IHash{}
+	password := "1234"
+	GivenHashedPassword(password, hashedPassword, err)
 	otpService := &mocks.IOtpService{}
-	otpService.On("GetCurrentOtp", "joey").Return("123456", nil)
+	otp := "123456"
+	otpService.On("GetCurrentOtp", name).Return(otp, err)
 	notification := &mocks.INotification{}
-	notification.On("Notify", "joey").Return(nil)
+	notification.On("Notify", name).Return(err)
 	logger := &mocks.ILogger{}
 	authService = AuthenticationService.NewAuthenticationService(failedCounter, profile, hash, otpService, notification, logger)
 	exitCode := m.Run()
 	os.Exit(exitCode)
+}
+
+func GivenHashedPassword(password string, hashedPassword string, err error) *mock.Call {
+	return hash.On("Compute", password).Return(hashedPassword, err)
+}
+
+func GivenPasswordFromDB(name string, hashedPassword string, err error) *mock.Call {
+	return profile.On("GetPasswordFromDB", name).Return(hashedPassword, err)
+}
+
+func GivenResetReturn(name string, err error) *mock.Call {
+	return failedCounter.On("Reset", name).Return(err)
+}
+
+func GivenFailedCount(name string, failedCount int, err error) *mock.Call {
+	return failedCounter.On("GetFailedCount", name).Return(failedCount, err)
+}
+
+func GivenAddFailedCountReturn(name string, err error) *mock.Call {
+	return failedCounter.On("AddFailedCount", name).Return(err)
+}
+
+func GivenAccountIsLocked(name string, isLocked bool, err error) *mock.Call {
+	return failedCounter.On("GetLock", name).Return(isLocked, err)
 }
 func Test_isValid(t *testing.T) {
 	isValid, err := authService.Verify("joey", "1234", "123456")
